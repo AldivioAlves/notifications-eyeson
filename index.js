682234f0004f21fa4b8c607bbff404cdb5c8e1a9
AldivioAlves <42225516+AldivioAlves@users.noreply.github.com>
2020-12-29 22:47:03 -0300

    config cron

--- index.js
+++ index.js
@@ -10,29 +10,25 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-const job = new CronJob('5 * * * * *', () => {
+const job = new CronJob('*/30 * * * * *', () => {
     notifications
     .sendToTopic(
         'sync', // device fcm tokens...
         {
           data: {
-            owner:'eu beti',
-            user: 'má oe',
-            picture:'nao tem hein',
+            context:'syncronization'
           },
         },
         {
-          // Required for background/quit data-only messages on iOS
           contentAvailable: true,
-          // Required for background/quit data-only messages on Android
           priority: 'high',
+          timeToLive:1
         },
       );
 })
 job.start()
 
 const sendNotifications = (req, res) => {
-    console.log("Sera encaminhada uma nova mensagem para os usuarios!!!")
     const { notebookUuid, userName, notebookName, userToken } = req.body
     const message = {
         topic: notebookUuid,
@@ -43,12 +39,7 @@ const sendNotifications = (req, res) => {
         }
     }
 
-    const subscribeUser = () => {
-        notifications
-            .subscribeToTopic(userToken, notebookUuid)
-            .then(res => console.log('sucesso em inscrever novamente o user'))
-            .catch(e => console.log('erro em inscrever novamente o user ', e))
-    }
+
     console.log('user token ', userToken)
     notifications
         .unsubscribeFromTopic(userToken, notebookUuid)
@@ -58,15 +49,14 @@ const sendNotifications = (req, res) => {
                 .then((value) => {
                     console.log('mensagem enviada com sucesso!')
                     res.status(200).send("Notificação enviada com sucesso!")
-                    subscribeUser()
+                    notifications.subscribeToTopic(userToken, notebookUuid)
                 })
                 .catch((e) => {
-                    console.log('error ao enviar a mensagem!', e)
                     res.status(500).send("Notificação não enviada!")
-                    subscribeUser()
+                    notifications.subscribeToTopic(userToken, notebookUuid)
                 })
         })
-        .catch((e) => console.log('erro ao cancelar a inscrisao ', e))
+        .catch((e) =>{})
 
 }
 
@@ -74,4 +64,4 @@ app.post('/notifications', sendNotifications)
 
 const port = 3000
 
-app.listen(process.env.PORT || port, () => console.log("servico executando na porta =>", port))
+app.listen(process.env.PORT || port, () => console.log("running"))
