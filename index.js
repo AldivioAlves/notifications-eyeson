d33a0dfc93f8ffaaa1214014a2836284119afea8
AldivioAlves <42225516+AldivioAlves@users.noreply.github.com>
2021-01-07 09:18:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
-const job = new CronJob('*/30 * * * * *', () => {
+const job = new CronJob('* */2 * * * *', () => {
     notifications
     .sendToTopic(
         'sync', // device fcm tokens...
